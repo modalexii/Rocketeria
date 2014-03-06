8bfb67c1ec1f81411df4185eb9c2f56fedbd7900
modalexii <kyle@unsecu.re>
2014-03-06 00:28:16 -0500

    unused/non-functional template. May be discarded or used for form validation

--- FSClient/static/client_info_form.js
+++ FSClient/static/client_info_form.js
@@ -1,12 +1,17 @@
-'children' : $('input[name="custom-student"]').val(),
-				'holder1_first' : $('input[name="holder1_first"]').val(),
-				'holder1_last' : $('input[name="holder1_last"]').val(),
-				'holder2_first' : $('input[name="holder2_first"]').val(),
-				'holder2_last' : $('input[name="holder2_last"]').val(),
-				'phone' : $('input[name="phone"]').val(),
-				'email' : $('input[name="email"]').val(),
-				'street1' : $('input[name="street1"]').val(),
-				'street2' : $('input[name="street2"]').val(),
-				'city' : $('input[name="city"]').val(),
-				'state' : $('input[name="state"]').val(),
-				'postal_code' : $('input[name="postal_code"]').val(),
+$(document).ready(function(){
+	children = []
+	$('.testimonial').each(function(i, obj) {
+     	children.push(i);
+	});
+	'holder1_first' : $('input[name="holder1_first"]').val(),
+	'holder1_last' : $('input[name="holder1_last"]').val(),
+	'holder2_first' : $('input[name="holder2_first"]').val(),
+	'holder2_last' : $('input[name="holder2_last"]').val(),
+	'phone' : $('input[name="phone"]').val(),
+	'email' : $('input[name="email"]').val(),
+	'street1' : $('input[name="street1"]').val(),
+	'street2' : $('input[name="street2"]').val(),
+	'city' : $('input[name="city"]').val(),
+	'state' : $('input[name="state"]').val(),
+	'postal_code' : $('input[name="postal_code"]').val(),
+}
