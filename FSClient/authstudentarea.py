2399c44c33241be70114c5769c68720e2230667f
modalexii <kyle@unsecu.re>
2014-03-09 01:19:26 -0500

    modified breakout of client names to account for fs_client being None

--- FSClient/authstudentarea.py
+++ FSClient/authstudentarea.py
@@ -23,21 +23,23 @@ class AuthStudentHandler(webapp2.RequestHandler):
         webcode += 'Hello <em>%s</em>! ' % (federated_nick)
         webcode += ''' [<a href="%s">Sign Off</a>]''' % logouturl
         webcode += '''</p>'''
+        webcode += '''<p><div id="booknow_link">Book A Lesson</div></p><hr/>'''
 
         fs_client = clients.fs_client_from(federated_email)
         rock_client = clients.RockClient(fs_client,federated_user)
 
-        eventlist = []
-        
-        fs_range = ranges.getfsrange(offset=0,num_weeks=num_weeks) # always num_weeks=1
-        #print "\n\nFS_RANGE FROM AUTHSTUDENTAREA: ",fs_range
-        
-        events_attended = rock_client.events_attended(fs_range)
-        #print "\n\nEVENTS_IN_RANGE #%s from AUTHSTUDENTAREA: " % n,events_in_range
+        if fs_client:
+            eventlist = []
 
-        webcode += '''              <div id="calendar"> '''
-        webcode += events.makecal(eventlist=events_attended,num_weeks=num_weeks,labels="existing")
-        webcode += '''              </div> '''
+            fs_range = ranges.getfsrange(offset=0,num_weeks=num_weeks) # always num_weeks=1
+            #print "\n\nFS_RANGE FROM AUTHSTUDENTAREA: ",fs_range
+        
+            events_attended = rock_client.events_attended(fs_range)
+            #print "\n\nEVENTS_IN_RANGE #%s from AUTHSTUDENTAREA: " % n,events_in_range
+
+            webcode += '''              <div id="calendar"> '''
+            webcode += events.makecal(eventlist=events_attended,num_weeks=num_weeks,labels="existing")
+            #webcode += '''              </div> ''' # aah there is an extra div somewhere in makecal() fix this shiiiit
 
         webcode += htmlblob.get("rocketeria-tail",)
 
