2043a4a14644fe3200b3df9bb157ca0cc2bbe93f
modalexii <kyle@unsecu.re>
2014-03-09 01:27:38 -0500

    why am i committing this ??

--- FSClient/testscript.py
+++ FSClient/testscript.py
@@ -11,15 +11,33 @@ class TestHandler(webapp2.RequestHandler):
 		'''handle HTTP GETs'''
 		self.response.headers['Content-Type'] = 'text/html'
 		self.response.write('''<hmtl><body>''')
-		import fsapi,json,pprint,clients
+		import fsapi,json,pprint,clients,rockapi
+		from google.appengine.api import urlfetch
 
-		client = clients.fs_client_from('kylec.steely@gmail.com')
+		self.response.write('''
+			<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+			<script>
+			$(document).ready(function(){
+				function send_data() {
+					$.post("/api", {
+						"request" : "cancel", 
+						"id" : "8gUW60F2b4",
+						"fsat" : "2014-03-10T16:00:00-0500"
+					})	
+					.done(function(data) {
+						concole.log(ok);
+					})
+					.fail(function(textStatus, errorThrown) {
+						console.log(textStatus + ': ' + errorThrown); // temp debugging measure
+					});
+				}
 
-		post = {}
-
-		bookings = fsapi.apirequest('bookings',post)
-
-		self.response.write(bookings.content)
+				console.log("about to send data...");
+				send_data();
+				console.log("should have sent data.");
+			});
+			</script>
+			''')
 
 		self.response.write('''</body></html>''')
 
