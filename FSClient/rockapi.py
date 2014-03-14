53dc81b8c9179d3e5dc7cd5c43559e46d1ab43ca
modalexii <kyle@unsecu.re>
2014-03-13 23:48:24 -0400

    modified for new fs_datetime use; modified to use new templating system

--- FSClient/rockapi.py
+++ FSClient/rockapi.py
@@ -31,28 +31,32 @@ class RockAPIHandler(webapp2.RequestHandler):
 			selserviceid = cgi.escape(self.request.get('serviceid'))
 			selemployeeid = cgi.escape(self.request.get('employeeid'))
 
-			if selemployeeid == 'Any':
-				selemployeeid = None
-
 			fsrange = ranges.getfsrange(offset=0,num_weeks=3)
 			fsafter = fs_datetime.fullslateify(fsrange["after"], "%Y%m%dT000000Z")
 			fsbefore = fs_datetime.fullslateify(fsrange["before"], "%Y%m%dT115959Z")
 
-			post = {'service' : selserviceid, 'employee' : selemployeeid, 'after' : fsafter, 'before' : fsbefore}
+			post = {'service' : selserviceid, 'after' : fsafter, 'before' : fsbefore}
+			if selemployeeid != "Any":
+				post.update({"employee" : selemployeeid})
+
 			post = json.dumps(post)
+			#print "POST DESTINED FOR FULLSLATE API FROM ROCKAPI GETOPENINGS REQUEST: ",post
 
 			fs_openings = fsapi.apirequest('openings',post)
 			fs_openings = json.loads(fs_openings.content)
+			#print "\nFS_OPENINGS FROM ROCKAPI GETOPENINGS REQUEST: ",fs_openings
 
 			rock_openings = []
 			for o in fs_openings["openings"]:
 				# make datetime opject for openings.py, and convert to EST
-				rock_openings.append(fs_datetime.normalize(o,"%Y%m%dT%H%M%SZ","GMT","EST"))
+				rock_openings.append(fs_datetime.normalize(o,"%Y%m%dT%H%M%SZ","GMT","America/New_York"))
 
 			#print "ROCKOPENINGS  FROM ROCKAPI: ",rock_openings
 			self.response.write(events.makecal(rock_openings,3,"new"))
 		elif request == u"client_form":
 			'''
+			er, is this used by anything?
+			me thinks this can be removed...
 			'''
 			from google.appengine.api import users
 			import json
@@ -70,13 +74,17 @@ class RockAPIHandler(webapp2.RequestHandler):
 			rock_client = clients.RockClient(fs_client,federated_user)
 			data = rock_client # pass the client object to pre-fill form fields
 
-			webcode = htmlblob.get("client_info_form",data)
+			webcode = rock_client.prefilled_form() #htmlblob.get("client_info_form",data)
 
 			self.response.write(webcode)
 		elif request == 'book':
 			'''
-			Confirm logged in, confirm we have all necessary info, 
+			Confirm we have all necessary info, 
 			then book an appointment with FullSlate
+
+			User must be logged in or else stuff will just break 
+			when Book Now is clicked. In future milestones, we can 
+			allow anonymous schedule checking
 			'''
 			from google.appengine.api import users
 			import fsapi,clients,htmlblob
@@ -84,10 +92,6 @@ class RockAPIHandler(webapp2.RequestHandler):
 			# headers set below as we figure out what exactly we're returning
 
 			federated_user = users.get_current_user()
-
-			if not federated_user: # not logged in - display login
-				self.response.write(htmlblob.get("federated_login"))
-				return
 			
 			fs_client = clients.fs_client_from(federated_user.email())
 			rock_client = clients.RockClient(fs_client,federated_user)
@@ -106,7 +110,7 @@ class RockAPIHandler(webapp2.RequestHandler):
 					print 'FULLSLATE DIDNT LIKE THE BOOKING REQUEST: ',booking_response.status_code,' ',booking_response.content
 					print 'SENDING CLIENT INFO FORM...'
 					self.response.headers['Content-Type'] = 'text/html'
-					self.response.write(htmlblob.get("client_info_form",rock_client))
+					self.response.write(rock_client.prefilled_form())
 					return
 			else:
 				print 'FINALLY, POST LOOKS LIKE THIS: ',post,'\nLETTING IT GO TO FULLSLATE...'
