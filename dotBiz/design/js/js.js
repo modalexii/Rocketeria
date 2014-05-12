49b4e60191acae3f32d83aaab862709ec7ed4ea0
vagabond251 <greg.englar@gmail.com>
2014-05-12 15:27:58 -0400

    5/12/2014 - 3:22 PM
    -Removed border around submit objects on click
    -Added fallback for banner for non-Javascript browsers
    -Added effect for clicking of billboard nav arrows

--- dotBiz/design/js/js.js
+++ dotBiz/design/js/js.js
@@ -5,7 +5,32 @@ $(".newsletter-email").click(function (){
 $(this).val("");
 });
 
-
-
+$(".arrow-next").mouseenter(function() {
+$(this).css("background", "url('images/a03.png') -186px -35px no-repeat");
+})
+.mouseleave(function(){
+$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
 });
 
+$(".arrow-next").mousedown(function() {
+$(this).css("background", "url('images/a03.png') -66px -35px no-repeat");
+})
+.mouseup(function(){
+$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
+});
+
+$(".arrow-previous").mouseenter(function() {
+$(this).css("background", "url('images/a03.png') -125px -35px no-repeat");
+})
+.mouseleave(function() {
+$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
+});
+
+$(".arrow-previous").mousedown(function(){
+$(this).css("background", "url('images/a03.png') -5px -35px no-repeat");
+})
+.mouseup(function(){
+$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
+});
+
+});
