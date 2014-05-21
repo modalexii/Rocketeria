cf1f04c033386f4f5061204a1f3a3c5f89bfe3cf
modalexii <kyle@unsecu.re>
2014-05-20 22:52:34 -0400

    er, apparently indented some stuff for no reason?

--- dotBiz/dev/gae_db.py
+++ dotBiz/dev/gae_db.py
@@ -3,17 +3,18 @@ import logging
 
 
 class Page(ndb.Model):
-    uri = ndb.StringProperty(required = True)
-    content = ndb.GenericProperty(indexed = False, required = True)
+	uri = ndb.StringProperty(required = True)
+	content = ndb.GenericProperty(indexed = False, required = True)
 
 def add_or_update_page(uri, content):
-    new_page = Page(id = uri, uri = uri, content = content)
-    key = new_page.put()
-    return key
+	new_page = Page(id = uri, uri = uri, content = content)
+	key = new_page.put()
+	return key
 
 def delete_page(uri):
-    ndb.Key(Page, uri).delete()
+	ndb.Key(Page, uri).delete()
 
 def fetch_content(uri):
-    target = Page.get_by_id(uri)
-    return target.content
+	logging.info("DATAFASE FETCH: " + uri)
+	target = Page.get_by_id(uri)
+	return target.content
