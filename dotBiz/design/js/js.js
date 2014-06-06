57d99212a1bb7a6da9147a3a0ea4b5e4f4ad0b60
modalexii <kyle@unsecu.re>
2014-06-06 00:25:48 -0400

    Modified mouseup event to keep arrows visible after click

--- dotBiz/design/js/js.js
+++ dotBiz/design/js/js.js
@@ -5,31 +5,31 @@ $(document).ready(function() {
 	//
 
 	$(".arrow-next").mouseenter(function() {
-		$(this).css("background", "url('images/a03.png') -186px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') -186px -35px no-repeat");
 	})
 	.mouseleave(function(){
-		$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') 55px -35px no-repeat");
 	});
 
 	$(".arrow-next").mousedown(function() {
-		$(this).css("background", "url('images/a03.png') -66px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') -66px -35px no-repeat");
 	})
 	.mouseup(function(){
-		$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') -186px -35px no-repeat");
 	});
 
 	$(".arrow-previous").mouseenter(function() {
-		$(this).css("background", "url('images/a03.png') -125px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') -125px -35px no-repeat");
 	})
 	.mouseleave(function() {
-		$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') 55px -35px no-repeat");
 	});
 
 	$(".arrow-previous").mousedown(function(){
-		$(this).css("background", "url('images/a03.png') -5px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') -5px -35px no-repeat");
 	})
 	.mouseup(function(){
-		$(this).css("background", "url('images/a03.png') 55px -35px no-repeat");
+		$(this).css("background", "url('/static/image/a03.png') -125px -35px no-repeat");
 	});
 
 	$("#lightbox-shadow").click(function() {
