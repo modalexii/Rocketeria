8354054a636cb1e2af9faaedd3676f6f20c4e607
modalexii <kyle@unsecu.re>
2014-06-06 00:13:20 -0400

    Add ability to pass different dir & extenstion

--- dotBiz/dev/templates.py
+++ dotBiz/dev/templates.py
@@ -1,27 +1,7 @@
-def get(name, extension = "html"):
+def get(name, extension = "html", bin = "html"):
 	'''
-	Fetch a file from a child dir of templates with the same name as the file extension.
+	Fetch a file from a child dir of templates
 	'''
 
-	with open("templates/{extension}/{name}.{extension}".format(**locals()),"r") as t:
+	with open("templates/{bin}/{name}.{extension}".format(**locals()),"r") as t:
 		return t.read()
-
-
-'''
-This was used when we thought embedded-everything was cool.
-It might have utility in the future.
-def get(name, tag = None, tag_type = None, extension = "html", comment = False):
-
-	with open("templates/{extension}/{name}.{extension}".format(**locals()),"r") as t:
-		content = t.read()
-
-	if tag and tag_type:
-		content = '<{tag} type="{tag_type}">{content}</{tag}>'.format(**locals())
-	elif tag and not tag_type:
-		content = '<{tag}>{content}</{tag}>'.format(**locals())
-
-	if comment:
-		content = '<!-- {content} -->'.format(**locals())
-
-	return content
-'''
