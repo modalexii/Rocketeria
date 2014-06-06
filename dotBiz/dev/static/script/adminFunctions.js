83b74689460b46e50bfbc9d27bde3443b466a69f
modalexii <kyle@unsecu.re>
2014-06-06 00:00:57 -0400

    Modified for sub page content

--- dotBiz/dev/static/script/adminFunctions.js
+++ dotBiz/dev/static/script/adminFunctions.js
@@ -1,8 +1,24 @@
 $(document).ready(function(){
 
+	if (!String.format) {
+		// stackoverflow.com/questions/610406/javascript-equivalent-to-printf-string-format
+		String.format = function(format) {
+			var args = Array.prototype.slice.call(arguments, 1);
+			return format.replace(/{(\d+)}/g, function(match, number) { 
+				return typeof args[number] != 'undefined'
+					? args[number] 
+					: match
+				;
+			});
+		};
+	}
+
 	$('head').append('<link rel="stylesheet" type="text/css" href="/static/style/admin_bar.css">');
+	$('head').append('<script type="text/javascript" src="/static/script/ckeditor/ckeditor.js"></script>');
 
 	function enterEditingMode(newPath) {
+		$('#content-sub').prop('contenteditable',true);
+		$('#header-sub h1').prop('contenteditable',true);
 		$('#editthis').removeClass('admin_link');
 		$('#editthis').html(
 			'Save to: ' + window.location.hostname + '/' +
@@ -11,7 +27,6 @@ $(document).ready(function(){
 			'<div id="cancel" class="admin_link">Cancel</div>'
 		);
 
-		$('#editable').addClass('green_border');
 		$('#editthis').addClass('yellow_bg');
 		$('#cancel').removeClass('yellow_bg').addClass('red_bg');
 		$('#deletethis').hide();
@@ -20,6 +35,30 @@ $(document).ready(function(){
 		$('#newpage').off('click');
 		$('#bannersettings').hide();
 		$('#bannersettings').off('click');
+		$('#signout').hide();
+		$('#signout').off('click');
+
+		// Do not allow headers on these pages to change color
+		var locked_header_color = ['/lessons','/store','/rentals','/events','/testimonials']
+		if ($.inArray(window.location.pathname, locked_header_color) === -1) {
+			$("img.bg-header-sub").on('click', function() {
+				if ($("img.bg-header-sub").attr('src') === '/static/image/header-green.png') {
+					$("img.bg-header-sub").attr('src','/static/image/header-blue.png');
+				} 
+				else if ($("img.bg-header-sub").attr('src') === '/static/image/header-blue.png') {
+					$("img.bg-header-sub").attr('src','/static/image/header-yellow.png');
+				}
+				else if ($("img.bg-header-sub").attr('src') === '/static/image/header-yellow.png') {
+					$("img.bg-header-sub").attr('src','/static/image/header-red.png');
+				}
+				else if ($("img.bg-header-sub").attr('src') === '/static/image/header-red.png') {
+					$("img.bg-header-sub").attr('src','/static/image/header-purple.png');
+				}
+				else {
+					$("img.bg-header-sub").attr('src','/static/image/header-green.png');
+				}
+			});
+		}
 
 		$('#save').on('click', function() {
 			var resource = $.trim($('input[name="resource"]').prop('value'));
@@ -28,8 +67,21 @@ $(document).ready(function(){
 				$('input[name="resource"]').addClass('red_border');
 			}
 			else {
+				// the following assumes that all editable pages are identical
+				// content has to be recreated
+				// if it is captured with outerHTML, we get all the ckeditor junk too
+				var content_template = '<div id="header-sub"><img class="bg-header-sub" src="{0}" width="705px" height="82px"/><h1>{1}</h1></div><!-- /header-sub --><div id="content-sub">{2}</div><!-- /content-sub -->'
+				var content = String.format(
+					content_template,
+					$('img.bg-header-sub').attr('src'),
+					$('#header-sub h1').html(),
+					$('#content-sub').html()
+				);
+
+				console.log(content);
+
 				$.post('/modify/publish', {
-					'content' : $('#editable').html(),
+					'content' : content,
 					'resource' : resource,
 				})
 				.done(function() {
@@ -40,7 +92,6 @@ $(document).ready(function(){
 				});
 			}
 		});
-		$('#admin_bar').append('<script type="text/javascript" src="/static/script/ckeditor/ckeditor.js"></script>');
 		$('#cancel').on('click', function() {
 			if(window.location.pathname === '/modify/new') {
 				window.location.replace('/');
@@ -49,6 +100,10 @@ $(document).ready(function(){
 				window.location.replace(window.location.pathname);
 			}
 		});
+		// Turn off automatic editor creation first.
+		CKEDITOR.disableAutoInline = true;
+		CKEDITOR.inline( 'content-sub' );
+		//CKEDITOR.inline( 'content-sub' ); // + line for each div that should spawn a ck instance
 	}
 
 	$('#newpage').on('click', function() {
@@ -67,26 +122,28 @@ $(document).ready(function(){
 		window.open('/modify/info', 'upload', 'left=20,top=20,width=500,height=500,toolbar=0,menubar=0,resizable=0');
 	})
 
-
 	if(editable_existing){
 		$('#deletethis').click(function() {
-			$.post('/modify/delete', {
-				'resource' : window.location.pathname,
-			})
-			.done(function() {
-				window.location.replace('/');
-			})
-			.fail(function() {
-				alert('Sorry, the delete request didn\'t go through. Check your internet connection, refresh and try again. If this condition persists, please file a bug.');
-			});
+			if (confirm('Really delete ' + window.location.pathname + 'forever?')) {
+				$.post('/modify/delete', {
+					'resource' : window.location.pathname,
+				})
+				.done(function() {
+					window.location.replace('/');
+				})
+				.fail(function() {
+					alert('Sorry, the delete request didn\'t go through. Check your internet connection, refresh and try again. If this condition persists, please file a bug.');
+				});
+			}
+
 		});
 		$('#editthis').on('click', function() {
-			$('#editable').prop('contenteditable',true);
+			$('#content-sub').prop('contenteditable',true);
+			$('#header-sub h1').prop('contenteditable',true);
 			enterEditingMode(newPath = window.location.pathname.replace('/',''));
 		});
 	}
 	else if(new_editor) {
-		$('#editable').prop('contenteditable',true);
 		enterEditingMode(newPath = ' ');
 		$('#deletethis').hide();
 	}
