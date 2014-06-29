cb3474ad3f43fa01bfd00118776d21725a963271
modalexii <kyle@unsecu.re>
2014-06-29 10:01:17 -0400

    Add check to specialChars

--- dotBiz/dev/static/script/ckeditor/config.js
+++ dotBiz/dev/static/script/ckeditor/config.js
@@ -77,6 +77,8 @@ CKEDITOR.editorConfig = function( config ) {
 		{ name: 'about' }
 	];
 
+	config.specialChars = config.specialChars.concat( ['&#10003;'] )
+
 	// Remove some buttons provided by the standard plugins, which are
 	// not needed in the Standard(s) toolbar.
 	config.removeButtons = 'Underline,Subscript,Superscript';
@@ -85,5 +87,5 @@ CKEDITOR.editorConfig = function( config ) {
 	config.format_tags = 'p;h1;h2;h3;pre';
 
 	// Simplify the dialog windows.
-	config.removeDialogTabs = 'image:advanced;link:advanced';
+	//config.removeDialogTabs = 'image:advanced;link:advanced';
 };
