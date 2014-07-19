e5aa923baa6440288cad4431017a9bf7c7a698d3
modalexii <kyle@unsecu.re>
2014-07-19 15:12:37 -0400

    add as_hardcoded(), update version

--- dotBiz/dev/get_env.py
+++ dotBiz/dev/get_env.py
@@ -1,14 +1,17 @@
 def version():
-	return "0.90"
+	return "0.9.5.2"
 
 def from_url(url):
 	if "127.0" in url or "localhost" in url:
 		environment = "offline"
 	elif "dev" in url:
 		environment = "dev"
-	elif "//rocketeria.biz" in url:
+	elif "rocketeria.biz" in url:
 		environment = "prod"
 	else:
-		environment = "unknownInstance"
+		environment = "unknown_instance"
 
 	return environment
+
+def as_hardcoded():
+	return "dev"
