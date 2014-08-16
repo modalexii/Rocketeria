4ba976c83a6bc61b20f5680ca041eec8b14caa35
modalexii <kyle@unsecu.re>
2014-08-15 22:30:00 -0400

    add get_listing()

--- dotBiz/dev/gcs_wrapper.py
+++ dotBiz/dev/gcs_wrapper.py
@@ -1,9 +1,23 @@
 import webapp2
+import cloudstorage
 
 class FetchHandler(webapp2.RequestHandler):
 
+	def get_listing(self):
+		'''
+		Return HTML listing of GCS files.
+		'''
+		import pretty_listing
+		self.response.headers['Content-Type'] = 'text/html'
+
+		listing = cloudstorage.listbucket("/dres/")
+
+		self.response.write(
+			pretty_listing.make_html(listing)
+		)
+
+
 	def get(self, *args, **kwargs):
-		import cloudstorage
 		from urllib import unquote
 
 		uri = self.request.path
@@ -15,7 +29,11 @@ class FetchHandler(webapp2.RequestHandler):
 				mode = "r",
 			)
 		except cloudstorage.NotFoundError:
-			self.response.set_status(404)
+			if uri == "/dres/directory":
+				self.get_listing()
+				return # awk :-/
+			else:
+				self.response.set_status(404)
 		else:
 			gcs_object_info = cloudstorage.stat(
 				uri,
