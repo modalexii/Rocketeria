17d3d709ec331b45e1d33812b6b9072925432c26
modalexii <kyle@unsecu.re>
2014-08-24 13:46:33 -0400

    Add validation

--- dotBiz/dev/static/script/enroll.js
+++ dotBiz/dev/static/script/enroll.js
@@ -11,8 +11,55 @@ function addStudent() {
 	}
 }
 
+function highlight(field) {
+	$(field).css('background', '#f5ed14');
+}
+
+function unhighlightInputs(form) {
+	$('input[type="text"].form-input').css('background','none');
+	$('input[type="email"].form-input').css('background','none');
+}
+
+function scrollToBillTo() {
+	$('html, body').animate({
+		scrollTop: $("#billto_heading").offset().top
+	}, 800);
+}
+
 function validateEnroll() {
-	return false;
+	var validationError = false;
+
+	var exp = /.*[,| ].*$/;
+	if (!(exp.test($('input[name="billto_firstandlast"]').val()))) {
+		var fail = true;
+		highlight('input[name="billto_firstandlast"]');
+		validationError = true;
+	}
+
+	var exp = /^.+\@.+\..+$/;
+	if (!(exp.test($('input[name="billto_email"]').val()))) {
+		var fail = true;
+		highlight('input[name="billto_email"]');
+		validationError = true;
+	}
+
+	var exp = /^\s*(?:\+?(\d{1,3}))?[-. (]*(\d{3})[-. )]*(\d{3})[-. ]*(\d{4})(?: *x(\d+))?\s*$/;
+	if (!(exp.test($('input[name="billto_mobilephone"]').val()))) {
+		var fail = true;
+		highlight('input[name="billto_mobilephone"]');
+		validationError = true;
+	}
+
+	var exp = /^.* .*$/;
+	if (!(exp.test($('input[name="billto_address1"]').val()))) {
+		var fail = true;
+		highlight('input[name="billto_address1"]');
+		validationError = true;
+	}
+
+	if (validationError) {
+		scrollToBillTo();
+	}
 }
 
 $(document).ready(function() {
