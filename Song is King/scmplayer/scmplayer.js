b1510aa9c496431f9076607bbc76619a7d09ca4e
modalexii <kyle@unsecu.re>
2014-12-12 13:22:24 -0500

    create scmplyer direectory, move related files

---
+++ Song is King/scmplayer/scmplayer.js
@@ -0,0 +1,15 @@
+
+<?xml version="1.0" encoding="UTF-8" ?>
+<Module>
+    <ModulePrefs title="SCM Player" />
+    <Content type="html">
+     <![CDATA[
+       <script type="text/javascript">
+          <script type="text/javascript" src="https://bdd3f1b9e49e851c8b30111060bf7db44ce45af0.googledrive.com/host/0BwhrPB_buvbZWUIwRlV4bnVMbHc/scmapp.js" 
+data-config="{'skin':'https://bdd3f1b9e49e851c8b30111060bf7db44ce45af0.googledrive.com/host/0BwhrPB_buvbZWUIwRlV4bnVMbHc/scmskin.css','volume':60,'autoplay':false,'shuffle':true,'repeat':1,'placement':'top','showplaylist':false,'playlist':[{'title':'Metallica - Am I Evil','url':'http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Metallica/Metallica%20%27Am%20I%20Evil%27.mp3'},{'title':'Queen - Liar','url':'http://hcmaslov.d-real.sci-nnov.ru/public/mp3/Queen/Queen%20%27Liar%27.mp3'},{'title':'The Beatles - Eight Days a Week','url':'http://tylergrund.com/mp3/Beatles/Eight%20Days%20A%20Week.mp3'},{'title':'Excision - Crowd Control','url':'http://ftp.tambur.in/a/DRUMSTEP/Excision%20%20Downlink%20-%20Crowd%20Control%20(Delta%20Heavy%20Remix).mp3'}]}" ></script>
+
+      </script>
+      !
+     ]]>
+    </Content>
+</Module>
